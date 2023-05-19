425e9d4d3b45b31dc2f004b657ccd690ca7b5d1b
Anuwar-Hosain <anuwarhossn@gmail.com>
2023-05-19 17:53:34 +0600

    post route done

--- index.js
+++ index.js
@@ -29,6 +29,14 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const toysCollection = client.db("toyMarket").collection("allToys");
+
+    app.post("/post-toy", async (req, res) => {
+      const body = req.body;
+      console.log(body);
+      const result = await toysCollection.insertOne(body);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
